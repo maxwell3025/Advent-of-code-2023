97849af9ead0f32567f2a3f4b7397b40ac5c9f1d
Maxwell Tang <amoinus@gmail.com>
2023-12-03 23:42:47 -0500

    day 3 part 2

--- 3-dec/solution.ts
+++ 3-dec/solution.ts
@@ -1,6 +1,7 @@
 import * as fs from "node:fs/promises";
 const fileData = (await fs.readFile("./input")).toString();
 const dataList = fileData.trim().split('\n');
+const tags: number[][][] = []
 const width = dataList[0].length;
 const height = dataList.length;
 function getCharAt(x: number, y: number){
@@ -23,26 +24,24 @@ function readTag(x: number, y: number){
 	return parseInt(dataList[y].slice(x, x + numberLength(x, y)))
 }
 function isTag(x: number, y: number){
-	if(!isNumber(x, y)) return false;
+	if(!isNumber(x, y)) return;
 	console.log("found number", readTag(x, y))
 	for(let xx = x-1; xx < x + 1 + numberLength(x, y); xx++){
 		for(let yy = y-1; yy <= y+1; yy++){
-			if(!/[0-9.]/.test(getCharAtChecked(xx, yy))){
-				return true;
+			if(getCharAtChecked(xx, yy) === "*"){
+				console.log("match");
+				tags[yy] ??= [];
+				tags[yy][xx] ??= [];
+				tags[yy][xx].push(readTag(x, y))
 			}
-			console.log(getCharAtChecked(xx, yy))
 		}
 	}
-	console.log("bad")
-	return false;
 }
 
 let sum = 0;
 for(let x = 0; x < width; x++){
 	for(let y = 0; y < height; y++){
-		if(isTag(x, y)) sum += readTag(x, y)
+		isTag(x, y)
 	}
 }
-
-console.log(sum)
-
+console.log(tags.flat().filter(x => x.length === 2).map(([a, b]) => a*b).reduce((z, b) => z + b))
